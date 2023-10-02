0ec2af08748fd1fd7b4f1b07fbe7348758061fcc
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-10-03 02:57:42 +0600

    Create rocket slice and update axios

---
+++ src/redux/rockets/rocketSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+initialState = {
+  rockets: []
+}
+
+const rocketSlice = createSlice({
+  name: 'rocket',
+  initialState,
+  reducer: {},
+})
+
+export default rocketSlice;
