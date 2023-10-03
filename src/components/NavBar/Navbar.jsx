640ea1791fd6dbf81d0631bb133db64bbaa064e3
unknown <lilskyex0x.apk@gmail.com>
2023-10-03 14:56:27 +0630

    Create navbar folder

---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from 'react-router-dom';
+import '../NavBar/Navbar.css'
+
+const Navbar = () => (
+  <nav className="navbar">
+    <div className="logo-container">
+      <a href="/">
+        <img src="../planet.png" alt="logo" className="logo-img" />
+        Space Travelers&apos; Hub
+      </a>
+      <ul className="nav-menu">
+        <li className="nav-list">
+          <NavLink className="nav-link" to="/">Rockets</NavLink>
+        </li>
+        <li className="nav-list">
+          <NavLink className="nav-link" to="missions">Missions</NavLink>
+        </li>
+        <li className="nav-list">
+          <NavLink className="nav-link" to="my-profile">My Profile</NavLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
+
+export default Navbar;
