42698c2634f629e7fb3d0d775222b4cd28f21c02
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-10-03 18:18:28 +0600

    Conditional reserve badges

--- src/components/Spacecraft.jsx
+++ src/components/Spacecraft.jsx
@@ -17,7 +17,10 @@ const Spacecraft = () => {
           <li className="rocket-item" key={rocket.id}>
             <img src={rocket.flickr_images} alt="" />
             <h2 className="rocket-title">{rocket.name}</h2>
-            <p className="rocket-des">{rocket.description}</p>
+            <p className="rocket-des">
+              {!rocket.reserved && <span className="reserved-tag" style={{ color: 'red' }}>Reserved</span>}
+              {rocket.description}
+            </p>
             {rocket.reserved && (
             <button type="button" className="reserve-rocket" onClick={() => dispatch(reserveRocket(rocket.id))}>
               Reserve Rocket
