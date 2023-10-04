2d96fc4361c56881827d3bf143d39eb1a4f4eccc
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-10-04 21:09:25 +0600

    Structure the rocket list

--- src/components/Rocket/Spacecraft.jsx
+++ src/components/Rocket/Spacecraft.jsx
@@ -17,22 +17,26 @@ const Spacecraft = () => {
       {
         rockets.map((rocket) => (
           <li className="rocket-item" key={rocket.id}>
-            <img src={rocket.flickr_images} alt="" />
-            <h2 className="rocket-title">{rocket.name}</h2>
-            <p className="rocket-des">
-              {rocket.reserved && <span className="reserved-tag" style={{ color: 'red' }}>Reserved</span>}
-              {rocket.description}
-            </p>
-            {!rocket.reserved && (
+            <div className="img-container">
+              <img src={rocket.flickr_images} alt="" />
+            </div>
+            <div className="content-container">
+              <h2 className="rocket-title">{rocket.name}</h2>
+              <p className="rocket-des">
+                {rocket.reserved && <span className="reserved-tag" style={{ color: 'red' }}>Reserved</span>}
+                {rocket.description}
+              </p>
+              {!rocket.reserved && (
               <button type="button" className="reserve-rocket" onClick={() => dispatch(reserveRocket(rocket.id))}>
                 Reserve Rocket
               </button>
-            )}
-            {rocket.reserved && (
+              )}
+              {rocket.reserved && (
               <button type="button" className="cancel-reserve" onClick={() => dispatch(cancelRocket(rocket.id))}>
                 Cancel Reservation
               </button>
-            )}
+              )}
+            </div>
           </li>
         ))
       }
